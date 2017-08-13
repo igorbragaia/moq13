c4b5b0441c2f2bffeff1bfe34c5eb561d22655c8
Igor Bragaia <igor.bragaia@gmail.com>
2017-08-13 01:20:22 -0300

    vai!

---
+++ js/angular.js
@@ -0,0 +1,13 @@
+function MyCtrl($scope){
+
+	$scope.posts = [
+	  {
+	    "week": "2",
+	    "description": "Questão de Galileu e encontro de Romeu e Julieta."
+	  },
+	  {
+	    "week": "2",
+	    "description": "Questão de Galileu e encontro de Romeu e Julieta."
+	  }
+	];
+}
