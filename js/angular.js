6ae905f780ae0758506b9d23ba156073d11ee75f
Igor Bragaia <igor.bragaia@gmail.com>
2017-08-13 02:03:01 -0300

    update

--- js/angular.js
+++ js/angular.js
@@ -1,17 +1,19 @@
-function MyCtrl($scope){
+var myapp=angular
+		.module('myapp', [])
+		.controller("MyCtrl", function($scope){
 
-	$scope.posts = [
-	  {
-	    "week": "2",
-	    "description": "Questão de Galileu e encontro de Romeu e Julieta."
-	  },
-	  {
-	    "week": "2",
-	    "description": "Questão de Galileu e encontro de Romeu e Julieta."
-	  },
-	  {
-	    "week": "2",
-	    "description": "Questão de Galileu e encontro de Romeu e Julieta."
-	  }
-	];
-}
+			$scope.posts = [
+			  {
+			    "week": "2",
+			    "description": "Questão de Galileu e encontro de Romeu e Julieta."
+			  },
+			  {
+			    "week": "3",
+			    "description": "Questão de Galileu e encontro de Romeu e Julieta."
+			  },
+			  {
+			    "week": "4",
+			    "description": "Questão de Galileu e encontro de Romeu e Julieta."
+			  }
+			];
+});
