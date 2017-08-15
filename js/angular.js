2311887fa0b3cf2c25da4061794dc3b4b92d3219
Igor Bragaia <igor.bragaia@gmail.com>
2017-08-14 23:43:33 -0300

    semana 2

--- js/angular.js
+++ js/angular.js
@@ -7,11 +7,6 @@ var myapp=angular
 			    "week": "3",
 			    "description": "Questão de Galileu e encontro de Romeu e Julieta.",
 			    "url": "https://igorbragaia.github.io/moq13/reports/2.pdf"
-			  },
-			  {
-			    "week": "2",
-			    "description": "Questão de Galileu e encontro de Romeu e Julieta.",
-			    "url": "https://igorbragaia.github.io/moq13/reports/2.pdf"
 			  }
 			];
 });
